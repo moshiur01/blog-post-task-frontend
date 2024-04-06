051eee3b39d4e2b087ae9ef4f7d8ce3e65d4ba75
moshiur01 <mr.moshiur314@gmail.com>
2024-04-06 17:52:12 +0600

    conditonal blog post info added

--- src/components/UI/Loading.js
+++ src/components/UI/Loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-full">
+    <div className="flex items-center justify-center h-full">
       <span className="loading loading-dots loading-lg"></span>
     </div>
   );
