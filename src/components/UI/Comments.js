b7636724fe55dd0c74495824883565fd3a3eccc8
moshiur01 <mr.moshiur314@gmail.com>
2024-04-06 16:36:36 +0600

    comment delete added based on user role

---
+++ src/components/UI/Comments.js
@@ -0,0 +1,168 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import auth from "@/firebase/firebaase.auth";
+import axios from "axios";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import toast, { Toaster } from "react-hot-toast";
+import { FaUserAlt } from "react-icons/fa";
+import { MdDeleteForever, MdOutlineAccessTimeFilled } from "react-icons/md";
+
+const Comments = ({ comments, postId }) => {
+  const [user] = useAuthState(auth);
+
+  //   get user data
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    getUserData();
+  }, [user?.email]);
+
+  const getUserData = async () => {
+    try {
+      if (user?.email) {
+        const res = await axios.get(
+          `http://localhost:5000/api/v1/users/${user?.email}`
+        );
+
+        setUserId(res?.data?.data?.id);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  //handle tabs
+  const [activeTab, setActiveTab] = useState("reviews");
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  //data gather for post a comment
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  //post user comment
+  const onSubmit = async (data) => {
+    const finalData = { postId, ...data, userId };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/postComments/create-blogPost-comment",
+        finalData
+      );
+      if (response.data.success) {
+        toast.success("Comment added successfully");
+        window.location.reload();
+      }
+
+      reset();
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
+
+  //delete user comment
+  const handleCommentDelete = async (id) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:5000/api/v1/postComments/${id}`
+      );
+
+      //   console.log(res);
+      if (res?.data?.success) {
+        toast?.success("Comment Deleted successfully");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error submitting data:", error);
+    }
+  };
+  return (
+    <div className="container mx-auto my-8">
+      <div className="flex bg-gray-200 rounded-md mb-4">
+        {/*  Display Comments */}
+        <div
+          className={`px-4 py-2 cursor-pointer font-semibold ${
+            activeTab === "reviews" ? "bg-gray-300" : ""
+          }`}
+          onClick={() => handleTabChange("reviews")}
+        >
+          Comments
+        </div>
+
+        {/* Post a comment */}
+        {user?.email && (
+          <div
+            className={`px-4 py-2 cursor-pointer font-semibold ${
+              activeTab === "postReview" ? "bg-gray-300" : ""
+            }`}
+            onClick={() => handleTabChange("postReview")}
+          >
+            Post a Comment
+          </div>
+        )}
+      </div>
+
+      {/* Tab content */}
+      {activeTab === "reviews" && (
+        <div className="space-y-4">
+          <p className="text-2xl font-bold">Comments</p>
+          {comments?.map((comment, index) => (
+            <div key={index} className="bg-white shadow-md p-4 rounded-md">
+              <p className="text-gray-600 font-semibold flex items-center gap-3">
+                <FaUserAlt /> {comment?.user?.name}
+                {userId === comment?.user?.id && (
+                  <span
+                    className="cursor-pointer"
+                    onClick={() => {
+                      handleCommentDelete(comment?.id);
+                    }}
+                  >
+                    <MdDeleteForever />
+                  </span>
+                )}
+              </p>
+              <p className="text-gray-500 flex items-center gap-3">
+                <MdOutlineAccessTimeFilled />
+                {dayjs(comment?.createdAt)?.format("MMM D, YYYY, h:m A")}
+              </p>
+              <hr className="mt-3" />
+              <p>{comment?.commentBody}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {activeTab === "postReview" && (
+        <form
+          className="space-y-4 flex flex-col w-64"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <textarea
+            // value={reviewBody}
+            onChange={(e) => setReviewBody(e.target.value)}
+            placeholder="Write your comment here..."
+            className="textarea textarea-bordered"
+            rows={3}
+            required
+            {...register("commentBody", { required: true })}
+          />
+          <button type="submit" className="btn  btn-outline btn-sm w-20">
+            Submit
+          </button>
+        </form>
+      )}
+
+      <Toaster position="top-center" reverseOrder={false} />
+    </div>
+  );
+};
+
+export default Comments;
--- src/pages/posts/[postId].js
+++ src/pages/posts/[postId].js
@@ -1,64 +1,73 @@
 import RootLayout from "@/components/layout/RootLayout";
 import axios from "axios";
 import dayjs from "dayjs";
-import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import { BiCategoryAlt } from "react-icons/bi";
 import { FaRegComment } from "react-icons/fa6";
 import { MdDateRange } from "react-icons/md";
 import { RiProfileLine } from "react-icons/ri";
+
+import Comments from "@/components/UI/Comments";
+import auth from "@/firebase/firebaase.auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 const PostDetails = ({ post }) => {
+  const [user] = useAuthState(auth);
+
+  //handle tabs
+  const [activeTab, setActiveTab] = useState("reviews");
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center justify-center">
-      <Head>
-        <title>Blog posts Details</title>
-        <meta
-          name="description"
-          content="This is the details page of a blog post"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="sm:m-4">
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center justify-center">
+        {/* Image column */}
+        <div className="">
+          <Image
+            src={post?.img}
+            alt="Blog post image"
+            width={500}
+            height={500}
+            className="w-full h-auto"
+          />
+        </div>
 
-      {/* Image column */}
-      <div className="">
-        <Image
-          src={post?.img}
-          alt="Blog post image"
-          width={500}
-          height={500}
-          className="w-full h-auto"
-        />
-      </div>
+        {/* Body column */}
+        <div className="my-7">
+          <div>
+            <h1 className="text-3xl font-bold">{post?.title}</h1>
+            <div className="flex items-center gap-4 font-bold my-3">
+              <RiProfileLine /> {post?.user?.name}
+            </div>
+            <hr className="w-full h-2 bg-black border-0 mb-4" />
 
-      {/* Body column */}
-      <div className=" my-7">
-        <div>
-          <h1 className="text-3xl font-bold">{post?.title}</h1>
-          <div className="flex items-center gap-4 font-bold my-3">
-            <RiProfileLine /> {post?.user?.name}
+            <div className="flex gap-4 mb-3">
+              <div className="flex items-center gap-2">
+                <MdDateRange className="" />
+                <span className="text-gray-600">
+                  {dayjs(post?.createdAt)?.format("MMM D, YYYY")}
+                </span>
+              </div>
+              <div className="flex items-center gap-2 text-sm">
+                <FaRegComment />
+                <span className="text-gray-600">{post?.comments?.length}</span>
+              </div>
+              <div className="flex items-center gap-2 text-sm">
+                <BiCategoryAlt />
+                <span className="text-gray-600">{post?.category?.name}</span>
+              </div>
+            </div>
+            <p className="text-xl scroll-auto">{post?.description}</p>
           </div>
-          <hr className="w-full h-2 bg-black border-0 mb-4" />
-
-          <div className="flex gap-4 mb-3">
-            <div className="flex items-center gap-2">
-              <MdDateRange className="" />
-              <span className="text-gray-600">
-                {dayjs(post?.createdAt)?.format("MMM D, YYYY")}
-              </span>
-            </div>
-            <div className="flex items-center gap-2 text-sm">
-              <FaRegComment />
-              <span className="text-gray-600">{post?.comments?.length}</span>
-            </div>
-            <div className="flex items-center gap-2 text-sm">
-              <BiCategoryAlt />
-              <span className="text-gray-600">{post?.category?.name}</span>
-            </div>
-          </div>
-          <p className="text-xl scroll-auto">{post?.description}</p>
         </div>
       </div>
+
+      {/* Comments section */}
+      <Comments postId={post?.id} comments={post.comments} />
     </div>
   );
 };
